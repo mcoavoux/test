04cb2cc1a2487479dc330e092449d454f87e1610
Maximin Coavoux <mcoavoux@bollin.inf.ed.ac.uk>
2018-11-22 15:32:54 +0000

    up

---
+++ get_data.sh
@@ -0,0 +1,16 @@
+
+
+
+#wget https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-1827/ud-treebanks-v1.4.tgz
+
+#wget https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-2515/ud-treebanks-v2.1.tgz
+
+wget https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-1699/ud-treebanks-v1.3.tgz
+
+#tar xzf ud-treebanks-v1.4.tgz
+#tar xzf ud-treebanks-v2.1.tgz
+tar xzf ud-treebanks-v1.3.tgz
+
+
+
+
